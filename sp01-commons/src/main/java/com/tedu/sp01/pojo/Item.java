6635378a5b7161aeee148fc25af075b17e05283a
Administrator <Administrator@JQJLJDG6B4V3H09>
2019-06-28 14:20:19 +0800

    23323

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,27 @@
+package com.tedu.sp01.pojo;
+
+ 
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
+
+ 
+
+@Data
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+
+public class Item {
+
+      private Integer id;//21132141
+
+      private String name;//违法记录看过了
+
+      private Integer number;
+//217831924
+}
---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
@@ -0,0 +1,27 @@
+package com.tedu.sp01.pojo;
+
+ 
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
+
+ 
+
+@Data
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+
+public class User {
+
+      private Integer id;
+
+      private String username;//2
+
+      private String password;//1
+
+}
